ceabe70dba417069ac30ac5c6152851f4b70c666
murali-ui <muralikudupudi99@gmail.com>
2018-03-29 19:50:38 -0500

    package.son and updated quote button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [QuoteComponent],
+  bootstrap: [ AppComponent]
 })
 export class AppModule { }
